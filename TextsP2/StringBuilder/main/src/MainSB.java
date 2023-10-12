9b96a0a8cd5bcc2141b6d1dbc173f675a40a5fcc
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-10-12 12:04:11 +0200

    StringBuildeer  commit

--- TextsP2/StringBuilder/main/src/MainSB.java
+++ TextsP2/StringBuilder/main/src/MainSB.java
@@ -8,13 +8,29 @@ public class MainSB {
 
     // Declare the str1 instance without initialization
     StringBuilder y = new StringBuilder();
-    System.out.println(y); // 'y' is an empty string
+    //System.out.println(y); // 'y' is an empty string
     // using str1.append() method for initialization
     y.append("Hello World"); // adding the "Hello World" to the end of the original reference 'y'
-    System.out.println(y);
+   // System.out.println(y);
 
     y.append(" from Java course"); // StringBuilder instances are mutable
-    System.out.println(y);
+    //System.out.println(y);
+
+
+    // Create a StringBuilder object with capacity
+    StringBuilder z  = new StringBuilder(10);
+   // System.out.println(z);
+    // System.out.println("The Capacity of z is: " + z.capacity());
+    z.append("123456789A1");// System.out.println("The Capacity of z is: " + z.capacity());
+    //capacity without being predefined
+    StringBuilder k  = new StringBuilder();
+    System.out.println(k.capacity());
+    System.out.println(k.length());
+    k.append("abcdefgijklmnosbrt123456789qwert11pqwertzuiio");
+    System.out.println(k.capacity());
+    System.out.println(k.length());
+
+
   }
 
 }
